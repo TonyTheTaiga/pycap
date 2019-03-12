1063654a5c08c3066e72476a2d8316db3d3fc546
tonyla858 <tonyla858@gmail.com>
2019-03-11 23:47:57 -0700

    added support for printing all information from data

--- app/magic/info.py
+++ app/magic/info.py
@@ -7,4 +7,12 @@ from app.api import Market
 def info(ticker):
     market = Market() 
     data = market.getInfo(ticker)
-    click.echo(data)
+    ids = (list(data.values())[0]).keys()
+    for content in data.values():
+        for i in ids:
+            if i != 'quote':
+                print(str(i)+": "+str(content[i]))
+            else:
+                for quote_data in content['quote']['USD']:
+                    print(str(quote_data)+": "+str(content['quote']['USD'][quote_data]))
+        print('--------------------------------------')
