024636ef57b445f35df03c794b059e3b5691c2b2
tonyla858 <tonyla858@gmail.com>
2019-03-12 22:03:59 -0700

    generalized info method to function

--- app/magic/info.py
+++ app/magic/info.py
@@ -3,9 +3,8 @@ from app.api import Market
 
 
 @click.command()
-@click.argument('ticker',nargs=-1)
 @click.option('--curr', default="BTC", help='Enter A Currency Code, Default is BTC')
-@click.argument('')
+@click.argument('ticker',nargs=-1)
 def info(curr, ticker):
     market = Market() 
     data = market.getInfo(curr, ticker)
@@ -13,8 +12,8 @@ def info(curr, ticker):
     for content in data.values():
         for i in ids:
             if i != 'quote':
-                print(str(i)+": "+str(content[i]))
+                print(str(i)+": "+str(content[i])) 
             else:
-                for quote_data in content['quote']['USD']:
-                    print(str(quote_data)+": "+str(content['quote']['USD'][quote_data]))
+                for quote_data in content['quote'][curr]:
+                    print(str(quote_data)+": "+str(content['quote'][curr][quote_data]))
         print('--------------------------------------')
