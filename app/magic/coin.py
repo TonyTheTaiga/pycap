4546b8041a55bf048892c729cec25485a416e4be
tonyla858 <tonyla858@gmail.com>
2019-03-13 22:03:51 -0700

    this time adding coin.py

---
+++ app/magic/coin.py
@@ -0,0 +1,35 @@
+#Class to contain current information on the coins
+
+class Coin(object):
+
+    def __init__(self, curr, data):
+        #data consisting of a single dictionary entry of a coin
+
+        #solution 1: store each data point as seperate variables
+        self.name = data['name']
+        self.symbol = data['symbol']
+        self.circulating_supply = data['circulating_supply']
+        self.total_supply = data['total_supply']
+        self.max_supply = data['max_supply']
+        self.date_added = data['date_added']
+        self.num_market_pairs = data['num_market_pairs']
+        self.tags = data['tags']
+        self.platform = data['platform']
+        self.cmc_rank = data['cmc_rank']
+        #beneath is in quote dict
+        self.price = data['quote'][curr]['price']
+        self.volume_24h = data['quote'][curr]['volume_24h']
+        self.percent_change_1h = data['quote'][curr]['percent_change_1h']
+        self.percent_change_24h = data['quote'][curr]['percent_change_24h']
+        self.percent_change_7d = data['quote'][curr]['percent_change_7d']
+        self.market_cap = data['quote'][curr]['market_cap']
+        self.last_updated = data['quote'][curr]['last_updated']
+
+        #solution 2: store each data point within a single dictionary
+        self.coin_dict = {}
+        for content in data:
+            if content != 'quote':
+                self.coin_dict[content] = data[content]
+            else:
+                for quote_data in data['quote'][curr]:
+                    self.coin_dict[quote_data] = data['quote'][curr][quote_data]
